b6c1399b27609fb585c30803afd59315c1dd607d
Sun <sun.lau@kingcitytech.com>
2019-10-23 15:03:04 +0800

    push

--- src/services/Experiment_APIService.js
+++ src/services/Experiment_APIService.js
@@ -8,20 +8,61 @@ export class Experiment_APIService extends APIService {
         super(api_base_url);
         this.is_dummy = is_dummy;
     }
-    getChart(){
+    getValue(){
         var self = this;
         const url = `${this.api_base_url}/get.php`;
         var dummy_response = {
-            chart: [500,114,106,106,107,111,133,221,783,2478]
+            value:[
+                {
+                    x: 10,
+                    y: 500
+                }, 
+                {
+                    x: 15,
+                    y: 114
+                }, 
+                {
+                    x: 20,
+                    y: 106
+                }, 
+                {
+                    x: 25,
+                    y: 107
+                }, 
+                {
+                    x: 30,
+                    y: 111
+                }, 
+                {
+                    x: 35,
+                    y: 133
+                }, 
+                {
+                    x: 40,
+                    y: 133
+                }, 
+                {
+                    x: 45,
+                    y: 221
+                }, 
+                {
+                    x: 50,
+                    y: 783
+                }, 
+                {
+                    x: 55,
+                    y: 2478
+                }
+            ]
         };
         if(self.is_dummy){
             return new Promise(function(resolve){setTimeout(function(){resolve(dummy_response)},1000);});
         }else{
-            // return axios.get(url)
-            // .then(response => { 
-            //     console.log(response);
-            // })
-            return axios.get(url).then(response => response.data);
+            return axios.get(url)
+            .then(response => { 
+                console.log(response);
+            })
+            // return axios.get(url).then(response => response.data);
         }
     }
     setCommand(command, value){
@@ -30,16 +71,24 @@ export class Experiment_APIService extends APIService {
         var bodyFormData = new FormData();
         bodyFormData.set('command', command);
         bodyFormData.set('value', value);
-        return axios({
-            method: 'post',
-            url: url,
-            data: bodyFormData,
-            config: { headers: {'Content-Type': 'multipart/form-data' }}
-            })
-        .then(function (response) {
-            //handle success
-            console.log(response);
-        });
+        var dummy_response = {
+            status:"success"
+        };
+        
+        if(self.is_dummy){
+            return new Promise(function(resolve){setTimeout(function(){resolve(dummy_response)},1000);});
+        }else{
+            return axios({
+                method: 'post',
+                url: url,
+                data: bodyFormData,
+                config: { headers: {'Content-Type': 'multipart/form-data' }}
+                })
+            .then(function (response) {
+                //handle success
+                console.log(response);
+            });
+        }
     }
 
 }
--- src/views/Experiment.vue
+++ src/views/Experiment.vue
@@ -4,7 +4,7 @@
             <b-row>
                 <b-col cols="7">
                     <b-card>
-                            <b-img 
+                            <!-- <b-img 
                                 src="http://158.132.254.152:8081/?action=stream"
                                 fluid
                                 rounded
@@ -13,7 +13,7 @@
                                 src="http://158.132.254.152:8082/?action=stream"
                                 fluid
                                 rounded
-                            />
+                            /> -->
                             <line-chart 
                                 ref="experiment_chart"
                                 :chartOptions="chartOptions"
@@ -28,35 +28,35 @@
                     >
                         <p>Laser State</p>
                         <b-button-group>
-                            <b-button variant="" @click="set('laser','OFF')">OFF</b-button>
-                            <b-button variant="danger" @click="set('laser','RED')">Red</b-button>
-                            <b-button variant="success" @click="set('laser','GREEN')">Green</b-button>
+                            <b-button variant="" @click="setCommand('LASER','OFF')">OFF</b-button>
+                            <b-button variant="danger" @click="setCommand('LASER','RED')">Red</b-button>
+                            <b-button variant="success" @click="setCommand('LASER','GREEN')">Green</b-button>
                         </b-button-group>
                         <p>Gain</p>
                         <b-button-group>
-                            <b-button variant="warning" @click="set('gain','VERY_LOW')">Very Low</b-button>
-                            <b-button variant="warning" @click="set('gain','LOW')">Low</b-button>
-                            <b-button variant="warning" @click="set('gain','HIGH')">High</b-button>
-                            <b-button variant="warning" @click="set('gain','VERY_HIGH')">Very High</b-button>
+                            <b-button variant="warning" @click="setCommand('GAIN','VERY_LOW')">Very Low</b-button>
+                            <b-button variant="warning" @click="setCommand('GAIN','LOW')">Low</b-button>
+                            <b-button variant="warning" @click="setCommand('GAIN','HIGH')">High</b-button>
+                            <b-button variant="warning" @click="setCommand('GAIN','VERY_HIGH')">Very High</b-button>
                         </b-button-group>
                         <p>Slit Position</p>
                         <b-button-group>
-                            <b-button variant="info" @click="set('slit','LEFT')">
+                            <b-button variant="info" @click="setCommand('SLIT','LEFT')">
                                 <font-awesome-icon icon="angle-left" />
                                 Left
                             </b-button>
-                            <b-button variant="info" @click="set('slit','RIGHT')">
+                            <b-button variant="info" @click="setCommand('SLIT','RIGHT')">
                                 <font-awesome-icon icon="angle-right" />
                                 Right
                             </b-button>
                         </b-button-group>
                         <p>Distance D</p>
                         <b-button-group>
-                            <b-button variant="info" @click="set('distance','INCREASE')">
+                            <b-button variant="info" @click="setCommand('DISTANCE','INCREASE')">
                                 <font-awesome-icon icon="angle-up" />
                                 Increase
                             </b-button>
-                            <b-button variant="info" @click="set('distance','DECREASE')">
+                            <b-button variant="info" @click="setCommand('DISTANCE','DECREASE')">
                                 <font-awesome-icon icon="angle-down" />
                                 Decrease
                             </b-button>
@@ -67,8 +67,8 @@
                     <b-card
                         header="Control"
                     >
-                        <b-btn @click="measure">Measure</b-btn>
-                        <b-btn @click="exportData">Export</b-btn>
+                        <b-btn @click="setCommand('MEASURE','START')">Measure</b-btn>
+                        <b-btn @click="getValue()">Export</b-btn>
                     </b-card>
                 </b-col>
             </b-row>
@@ -82,8 +82,8 @@
 import moment from 'moment';
 import LineChart from '@/components/LineChart.vue'
 import {Experiment_APIService} from '@/services/Experiment_APIService';
-// const apiService = new Experiment_APIService(process.env.VUE_APP_BASE_URL, 'dummy');
-const apiService = new Experiment_APIService(process.env.VUE_APP_BASE_URL);
+const apiService = new Experiment_APIService(process.env.VUE_APP_BASE_URL, 'dummy');
+// const apiService = new Experiment_APIService(process.env.VUE_APP_BASE_URL);
 export default {
   name: 'experiment',
   components: {
@@ -92,16 +92,14 @@ export default {
   data:function(){
       return{
           chartData: {
-                labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
                 datasets: [{
-                label: 'Usage',
-                backgroundColor: [
-                    'rgba(0, 123, 255, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(134,192,255,1)'
-                ],
-                borderWidth: 1
+                    backgroundColor: [
+                        'rgba(0, 123, 255, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(134,192,255,1)'
+                    ],
+                    borderWidth: 1
                 }]
             },
             chartOptions: {
@@ -118,12 +116,9 @@ export default {
                     display: false,
                 },
                 scales: {
-                    xAxes:[
+                    xAxes:[ 
                         {
-                            type: 'time',
-                            time: {
-                                unit: 'minute'
-                            }
+                            type: 'linear'
                         }
                     ],
                     yAxes: [{
@@ -139,26 +134,25 @@ export default {
   mounted: function(){
         var self = this;
         console.log("mounted");
-        self.getChart();
-
+        self.$refs.experiment_chart.renderChart([]);
   },
   methods:{
-
-        getChart(){
+        getValue(){
+            console.log("getValue");
             var self = this;
-            apiService.getChart()
+            apiService.getValue()
             .then((response) => {
                 console.log("response.value");
                 console.log(response);
                 self.$refs.experiment_chart.renderChart(response.value);
             });
         },
-        set(command, value){
-
+        setCommand(command, value){
             apiService.setCommand(command, value)
             .then((response) => {
                 console.log("command is set");
-                console.log("response.value");
+                console.log("response");
+                console.log(response);
             });
         }
   }
